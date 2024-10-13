be4c6db5d42dd770f73915c7d10d881a233b8461
Wunst <matthiesbe@gmail.com>
2024-10-13 13:32:30 +0200

    Add delta

--- system/modules/dev/default.nix
+++ system/modules/dev/default.nix
@@ -11,6 +11,13 @@
   programs.git.enable = true;
 
   hm.programs.git = with settings.user.git; {
+    delta = {
+      enable = true;
+      options = {
+        syntax-theme = "gruvbox-dark";
+        line-numbers = true;
+      };
+    };
     enable = true;
     userName = name;
     userEmail = email;
